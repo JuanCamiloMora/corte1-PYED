2295142ee81e573aacc42eb2a559a1e7f38f6cdc
JuanCamiloMora <110946408+JuanCamiloMora@users.noreply.github.com>
2022-09-27 09:28:58 -0500

    TRABAJO adelnatado

---
+++ corte 2/prueba.js
@@ -0,0 +1,10 @@
+<script>  
+function down() {  
+ document.getElementById("ul-down").style.opacity = "100%";  
+ document.getElementById("ul-down").style.display = "block";  
+}  
+
+function over() {  
+ document.getElementById("ul-down").style.display = "none";  
+}  
+</script>  
